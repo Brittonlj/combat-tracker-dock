608f8c51b237cbfaeca30ef7ee72af33fbf4dbbb
Brittonlj <brittonlj@gmail.com>
2025-04-10 13:39:21 -0500

    Fixed bug

--- scripts/App/Vision5eIntegration.js
+++ scripts/App/Vision5eIntegration.js
@@ -2,5 +2,5 @@ const VISION5E_PRECISE_DETECTION_LEVELS = 2;
 
 export function tokenHasVision(token){
     // tokenHasVision is derived from the detectionLevel property used by the Vision5e module 
-    return !this.token?.detectionLevel || this.token?.detectionLevel === VISION5E_PRECISE_DETECTION_LEVELS;      
+    return !token.detectionLevel || token.detectionLevel === VISION5E_PRECISE_DETECTION_LEVELS;      
 }
